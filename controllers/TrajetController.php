52b74cafe8bbe8f3e2d483b7ae3acadb301bc80a
Doriann Bachelet <doriann.bak@gmail.com>
2025-10-08 18:32:17 +0200

    Ajout du model et du controller

---
+++ controllers/TrajetController.php
@@ -0,0 +1,17 @@
+<?php
+require_once "models/TrajetModel.php";
+class TrajetController
+    {
+        private $model;
+        public function __construct()
+        {
+            $this->model =new TrajetModel();
+        }
+        public function getAllTrajets()
+        {
+            $trajets= $this->model->getDBAllTrajets();
+            echo json_encode($trajets);
+        }
+    }
+
+?>
